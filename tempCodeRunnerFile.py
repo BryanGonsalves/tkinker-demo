f99ce7b2139bd56a397279bfc9405968aa63ba20
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-01 22:14:41 +0400

    4th commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,12 @@
 import tkinter as tk
 
-
-def hello(event):
-    display = tk.Label(text ="Hello User!")
-    display.grid(column=0,row=2)
-
-
 window = tk.Tk()
 window.title("My First Tkinker GUI")
 label_name = tk.Label(text = "Hello, this is my first Tkinter App!")
 button_name = tk.Button(window,text="Click Me!")
 label_name.grid(column=0,row=0)
 button_name.grid(column=1,row=0)
-button_name.bind("<Button-1>",hello)
 
-window.geometry('1000x600')
+
+window.geometry('1000x1000')
 window.mainloop()

fe3286e604f1fe7b913aca8d92127cc6d3e9f35a
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-01 22:05:26 +0400

    second commit

--- first-gui.py
+++ first-gui.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("My First Tkinker GUI")
-label_name = tk.Label(text = "hello")
+label_name = tk.Label(text = "hello, this is my first Tkinter App")
 label_name.grid(column=0,row=0)
 
 window.geometry('1000x600')

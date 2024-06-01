d35c42993efe8fbe2c4e50745daffbfd85cd9449
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-02 00:56:08 +0400

    5th commit

--- first-gui.py
+++ first-gui.py
@@ -4,7 +4,10 @@ import tkinter as tk
 def hello(event):
     display = tk.Label(text ="Hello User!")
     display.grid(column=0,row=2)
-
+def bye(event):
+    display2 = tk.Label(text="Bye User!")
+    display2.grid(column=0,row=2)
+    
 
 window = tk.Tk()
 window.title("My First Tkinker GUI")
@@ -13,6 +16,8 @@ button_name = tk.Button(window,text="Click Me!")
 label_name.grid(column=0,row=0)
 button_name.grid(column=1,row=0)
 button_name.bind("<Button-1>",hello)
+button_name.bind("<Button-2>",bye)
+
 
 window.geometry('1000x600')
 window.mainloop()

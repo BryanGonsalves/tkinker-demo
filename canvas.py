a2f762b7af53dc5c64b3f2406014e66ca92862a8
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 12:08:17 +0400

    added oval, arc

--- canvas.py
+++ canvas.py
@@ -4,10 +4,13 @@ window=tk.Tk()
 window.title(" My Window ")
 window.geometry("600x400")
 
-myCanvas = tk.Canvas(window, bg="lightblue",height=300, width=300)
+myCanvas = tk.Canvas(window, bg="lightblue", height=300, width=300)
 myCanvas.grid(row=0,column=0)
 
 myLine = myCanvas.create_line(50,70,220,40,fill="blue")
 
+myOval = myCanvas.create_oval(40, 75, 120,150,fill="green")
+
+myArc = myCanvas.create_arc(175, 145, 75,200, start=0,extent=220,fill="yellow")
 
 window.mainloop()

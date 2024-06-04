c28eb285b3118ac54531fdf9c30a06e89428774c
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 11:55:07 +0400

    added radiobuttons

--- registration.py
+++ registration.py
@@ -25,4 +25,12 @@ nationVar.set("Select")
 opMenu = tk.OptionMenu(window, nationVar, "US","UK", "India")
 opMenu.grid(column=1,row=3)
 
+genderLabel = tk.Label(text="Gender: ")
+genderLabel.grid(column=0,row=4)
+genderVar = tk.StringVar()
+R1 = tk.Radiobutton(window,text="Male",variable=genderVar, value='Male')
+R1.grid(column=1,row=4)
+R2 = tk.Radiobutton(window, text="Female", variable=genderVar, value='Female')
+R2.grid(column=2,row=4)
+
 window.mainloop()

c077cc128175c28e4ce004b9d0856b517fa92ec2
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 12:15:35 +0400

    added frames with padding

--- frame.py
+++ frame.py
@@ -5,9 +5,18 @@ window.title("My Window")
 window.geometry("300x300")
 
 frame1 = tk.Frame(window)
-frame1.grid(column=0,row=0)
+frame1.grid(column=0,row=0,padx=20)
 
-myLabel = tk.Label(frame1,text="Hello World")
+frame2 = tk.Frame(window)
+frame2.grid(column=1,row=0)
+
+myText = tk.Text(frame1,height=10,width=20)
+myText.grid(column=0,row=0)
+
+myLabel = tk.Label(frame2,text="Hello World")
 myLabel.grid(column=0,row=0)
+
+myButton = tk.Button(frame2, text ="Click Here")
+myButton.grid(column=0,row=1)
 
 window.mainloop()

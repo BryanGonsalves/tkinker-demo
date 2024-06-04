2f36188baedc77d97d111e85db83ddc7c8ef4b46
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 12:25:50 +0400

    added frames

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -3,4 +3,10 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Rock-Paper-Scissors")
 window.geometry("400x300")
+frame1= tk.Frame(window)
+frame1.grid(column=0,row=0)
+frame2 = tk.Frame(window)
+frame2.grid(column=0,row=1)
+
+
 window.mainloop()

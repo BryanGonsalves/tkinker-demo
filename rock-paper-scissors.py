5b533ac40f1cf1e5ef24dd83f1e04000b0ebab51
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 12:28:56 +0400

    designed 1st frame

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -8,5 +8,9 @@ frame1.grid(column=0,row=0)
 frame2 = tk.Frame(window)
 frame2.grid(column=0,row=1)
 
+text_area = tk.Text(master=frame1,height=12,width=30,padx=10,pady=10,bg="#CAD5E2")
+text_area.grid(column=0,row=0)
+text_area.insert(tk.END,"\n\nYour Score: 0 \n\nComputer Score: 0\n\nClick on Any Button to Start.")
+
 
 window.mainloop()

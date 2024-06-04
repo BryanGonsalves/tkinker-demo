50ff276ce66634be831cd4aa44bdc82c5b907c1e
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-06-04 12:01:46 +0400

    added line

--- canvas.py
+++ canvas.py
@@ -4,8 +4,10 @@ window=tk.Tk()
 window.title(" My Window ")
 window.geometry("600x400")
 
-myCanvas = tk.Canvas(window, bg="lightblue",
-           height=300, width=300)
+myCanvas = tk.Canvas(window, bg="lightblue",height=300, width=300)
 myCanvas.grid(row=0,column=0)
+
+myLine = myCanvas.create_line(50,70,220,40,fill="blue")
+
 
 window.mainloop()
